ab4f83c1f32a834a9c54bfbeafa80a78657dc986
Kanishkaa-M <kanishkaamaheshkumar@gmail.com>
2025-11-20 19:21:18 +0530

    first commit

---
+++ react-app/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+export default function Nav(){
+  return (
+    <header className="topbar">
+      <div className="brand">KANISHKAA</div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/achievements">Achievements</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/skills">Skills</Link>
+        <a href="https://drive.google.com/file/d/1F-shoLFBxNpLyeHWWCRwa7Yt3j9oObiz/view?usp=drivesdk" target="_blank" rel="noreferrer">Resume</a>
+        <Link to="/contacts">Contact</Link>
+      </nav>
+    </header>
+  )
+}
---
+++ react-app/src/pages/Education.jsx
@@ -0,0 +1,25 @@
+export default function Education(){
+  return (
+    <div className="about">
+      <h2> 🎓 Education--------</h2>
+      <div className="ebox">
+        <p><b>Course</b>: Bachelor of Engineering in Computer Science and Engineering</p>
+        <p><b>Institute Name</b>: K.S.R College of Engineering</p>
+        <p><b>Year of Study</b>: 2024-2028</p>
+        <p><b>CGPA</b>: 8.7</p>
+      </div>
+      <div className="ebox">
+        <p><b>Course</b>: Higher Secondary Education</p>
+        <p><b>Institute Name</b>: Kongunadu Matric Higher Secondary School</p>
+        <p><b>Year of Study</b>: 2023-2024</p>
+        <p><b>Percentage</b>: 82.3</p>
+      </div>
+      <div className="ebox">
+        <p><b>Course</b>: Secondary School Education</p>
+        <p><b>Institute Name</b>: Kongunadu Matric Higher Secondary School</p>
+        <p><b>Year of Study</b>: 2021-2022</p>
+        <p><b>Percentage</b>: 89.4</p>
+      </div>
+    </div>
+  )
+}
