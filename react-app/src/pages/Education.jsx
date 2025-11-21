ce28a19de215857545a4ba1f1c4e2ab74932fd21
Kanishkaa-M <kanishkaamaheshkumar@gmail.com>
2025-11-21 15:21:42 +0530

    Made changes with linking

--- react-app/src/pages/Education.jsx
+++ react-app/src/pages/Education.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 export default function Education(){
   return (
     <div className="about">
---
+++ react-app/src/pages/Projects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Projects(){
+  return (
+    <div className="about">
+      <h2>Projects-------</h2>
+      <div>
+        <p><b> Personal Portfolio</b></p>
+        <p>🔹Description:</p>
+        <p>A responsive and modern personal portfolio website designed to showcase my skills, education, and projects. It serves as my digital identity where I can share my journey, achievements, and technical growth. The website is built with a clean UI and smooth navigation for an engaging user experience.</p>
+        <p>🔹Tech Stack:</p>
+        <p>HTML, CSS</p>
+        <p>🔹Features:</p>
+        <p>Interactive home section with typing animation, About section, Education and skills sections, Project showcase with preview links, Resume download option, Contact section with direct links to LinkedIn, GitHub, and Gmail.</p>
+        <center>
+          <img src="/img/portfolio.jpeg" alt="portfolio preview" style={{width:500}} />
+        </center>
+      </div>
+    </div>
+  )
+}
