b2555bbf6c47e3aa70ad368a588d66e1db97c032
Kanishkaa-M <kanishkaamaheshkumar@gmail.com>
2025-11-27 10:13:55 +0530

    Added an intro component with styles

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './pages/global.css';
-import './components/nav.css';
-import './pages/home.css';
-import './pages/about.css';
-import './pages/education.css';
-import './pages/projects.css';
-import './pages/contacts.css';
+import './pages/global.css'
+import './components/nav.css'
+import './pages/home.css'
+import './pages/about.css'
+import './pages/education.css'
+import './pages/projects.css'
+import './pages/contacts.css'
 
 
-<global></global>
+// After (fixed)
+import Intro from './components/Intro.jsx'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+)
+
+function Root() {
+  const [showApp, setShowApp] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowApp(true)
+    }, 5000) // Show intro for 3 seconds
+    return () => clearTimeout(timer)
+  }, [])
+
+  return showApp ? (
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  ) : (
+    <Intro />
+  )
+}
