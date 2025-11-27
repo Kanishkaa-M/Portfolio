55371505d8d237b1f7b1a7da40ac4c3cde1963c1
Kanishkaa-M <kanishkaamaheshkumar@gmail.com>
2025-11-27 08:21:05 +0530

    made sepearate css files for each page

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './style.css'
+import './pages/global.css';
+import './components/nav.css';
+import './pages/home.css';
+import './pages/about.css';
+import './pages/education.css';
+import './pages/projects.css';
+import './pages/contacts.css';
+
+
+<global></global>
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
